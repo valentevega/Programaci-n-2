c89b35f2910cafb39783e9e0c3a492300502883a
Vega Gonzalez Valente Eugenio <valente.vega10@gmial.com>
2021-09-13 21:53:40 -0600

    a

--- Suma.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "add.cpp"
-
-int main ()
-{
-    std::cout << "La suma de 3 y 4 es: " << add(3,4) << "\n";
-    return 0;
-}
--- input.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int getInteger()
-{
-    std::cout << "Ingrese un entero: ";
-    int x{};
-    std::cin >> x;
-    return x;
-}
