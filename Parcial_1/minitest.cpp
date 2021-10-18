01df5c67876a57668e213713553c73d0f9d627b1
Vega Gonzalez Valente Eugenio <valente.vega10@gmial.com>
2021-10-17 22:03:47 -0600

    a

---
+++ Parcial_1/minitest.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Ingrese un numero menor a 10: ";
+    int x{};
+    std::cin >> x;
+    if (x < 10) and (x == 2 or x == 3 or x == 5 or x == 7)
+        std::cout << "Es un número primo";
+
+    else if 
+    else
+        td::cout << "El número es mayor a 10";
+
+    return 0;
+}   
