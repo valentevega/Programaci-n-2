65849e3dc2b93fdedecaec8e0ec0dfe97f74dc58
Vega Gonzalez Valente Eugenio <valente.vega10@gmial.com>
2021-10-24 03:25:56 -0600

    a

--- Parcial_1/Cajero/main.cpp
+++ Parcial_1/Cajero/main.cpp
@@ -1,41 +1,88 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
+//Función de Operaciones a realizar
+int Operacion( double Saldo )
+{
+    std::cout << "¿Qué operación quiere realizar? \n";
+    std::cout << "1. Retirar \n";
+    std::cout << "2. Depositar \n";
+    std::cout << "3. Consultar Saldo \n";
+    std::cout << "4. Transferir \n";
+    int OP{};
+    std::cin >> OP;
+    double cant{};
+
+    switch(OP)
+    {
+        case 1:
+            std::cout << "Tu saldo disponible es: " << Saldo << "\n";
+            std::cout << "¿Cuánto quieres retirar? \n";
+            std::cin >> cant;
+            Saldo = Saldo - cant;
+            std::cout << "Retiraste: " << cant << "\n";
+            std::cout << "Queda en tu cuenta: " << Saldo << "\n";
+            break;
+        case 2:
+            std::cout << "Tu saldo disponible es: " << Saldo << "\n";
+            std::cout << "¿Cuánto quieres depositar? \n";
+            std::cin >> cant;
+            Saldo = Saldo + cant;
+            std::cout << "Depositaste: " << cant << "\n";
+            std::cout << "Queda en tu cuenta: " << Saldo << "\n";
+            break;
+        case 3:
+            std::cout << "Tu saldo disponible es: " << Saldo << "\n";
+            break;
+        default:
+            std::cout << "Ingrese una opción de las mostradas. ";
+            break;
+           
+    }
+    return 0;
+}
 
 
 int main()
 {
-    std::string NumCuenta[4] = {"0123456789", "5678901234", "9870098700", "6543265432"};
-    std::string NIP_V[4] = {"0123", "5678", "9870", "6543"};
-    double Saldo_V[4] = {700, 1500.50, 5000, 350.25};
+    //Declaración de variables de prueba
+    std::vector<std::string> NumCuenta = { "0123456789", "5678901234", "9870098700", "6543265432" };
+    std::vector<std::string> NIP_V = { "0123", "5678", "9870", "6543" };
+    double Saldo_V[4] = { 700, 1500.50, 5000, 350.25 };
+    bool Terminado = false;
 
     std::cout << "Ingrese un numero de cuenta: ";
-    char Cuenta;
+    std::string Cuenta;
     std::cin >> Cuenta;
 
-    for (int i = 0; i < 4; i++)
+    //Ciclo For para Puntero de Vectores
+    int i = 0;
+    do
     {
-        
-        if ( account() = 1 )
+        //Comparación de Numero de Cuenta al Vector
+        if (Cuenta == NumCuenta[i])
         {
-            std::cout << "Ingrese su NIP: \n";
-            char NIP;
+            std::cout << "Ingrese su NIP: ";
+            std::string NIP;
             std::cin >> NIP;
-        
-            if ( NIP == NIP_V[i] )
+
+            //Comparación de NIP al Vector
+            if (NIP == NIP_V[i])
             {
-                std::cout << "¿Qué operación quiere realizar? ";
-                std::cout << "1. Retirar";
-                std::cout << "2. Depositar";
-                std::cout << "3. Consultar Saldo";
-                std::cout << "4. Transferir";
-                int OP{};
-                std::cin >> OP;
-            }
-            else
-            {
-                
+                double Saldo{};
+                Saldo = Saldo_V[i];
+
+                //llamada a la función de las Operaciones a realizar
+                Operacion(Saldo);
+                Terminado = true;
             }
         }
-    std::cout << i;
-    } 
+        i++;
+    } while (i < 4);
+    if ( Terminado != true )
+    {
+        std::cout << "El Número de Cuenta o NIP no coinciden o no fueron encontrados. ";
+    }
+    return 0;
 }
